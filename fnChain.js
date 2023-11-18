124ec748c4ff5b88b81bd29a93c3f1c09f27b1ec
rom61977 <rom61977@gmail.com>
2023-11-18 23:35:28 +0900

    threadOrder

--- fnChain.js
+++ fnChain.js
@@ -1,88 +1,104 @@
-//function format
-//function or promise.
-//first argument will always be arg, free to put additional argument.
 /**
- * fnAry = [
- *  {
- *   fn: function
- *   catch: function called when fn failed to resolve.
- *  }
- * ]
- *
- * finalAry = {
- *  fnFinal = function.
- *  fnFinalCatch = function called when fnFinal failed to resolve.
- * }
+ * Represents a chain of functions/promises executed sequentially with parameters provided.
+ * @class
+ * @classdesc The `FnChain` class iterates through parameters and executes a series of
+ * functions/promises with each parameter. The result of each execution is carried over
+ * to the next iteration. After all parameters are processed, it optionally executes a final
+ * function/promise provided in the `finalAry`.
  */
-
 class FnChain {
-  constructor(paramAry, functionAry, finalAry) {
+  /**
+   * Creates an instance of the `FnChain` class.
+   * @constructor
+   * @param {Array} paramAry - An array of parameters to be used as arguments in function execution.
+   * @param {Array} functionAry - An array of functions/promises to be executed in sequence.
+   * @param {Object}
+   * @param {Object} [final] - An optional object containing finalization functions.
+   * @param {Function} [final.fn] - The final function to be executed after processing all parameters.
+   * @param {Function} [final.fnCatch] - The function to handle errors in the finalization process.
+   * @throws Will throw an error if `paramAry` or `functionAry` is empty.
+   */
+  constructor(paramAry, functionAry, finalObj) {
     if (paramAry.length === 0) throw new Error(`Parameter array is empty.`);
     if (functionAry.length === 0) throw new Error(`Function array is empty.`);
 
-    this.params = params.reverse();
+    this.params = paramAry.reverse();
     this.functionAry = functionAry.reverse();
-    this.carriedValue = null;
+    this.returns = [];
 
     this.functionAry = functionAry;
-    if (finalAry) {
-      this.fnFinal = finalAry.fnFinal ? finalAry.fnFinal : () => {};
-      this.fnFinalCatch = finalAry.fnFinalCatch
-        ? finalAry.fnFinalCatch
-        : () => {};
+    if (finalObj) {
+      this.fnFinal = finalObj.fn ? finalObj.fn : () => {};
+      this.fnFinalCatch = finalObj.fnCatch ? finalObj.fnCatch : () => {};
 
-      const params = [...this.params];
       const functions = [...this.functionAry];
-      this._recur(params, functions);
+      const param = this.params.pop();
+      this._recur(param, functions);
     }
   }
 
-  _recur = (params, functions) => {
+  /**
+   * Internal recursive function for executing functions/promises in sequence with parameters.
+   * @private
+   * @param {*} param - The current parameter to be used as an argument.
+   * @param {Array} functions - The functions/promises to be executed in a sequence.
+   */
+  _recur = (param, functions) => {
     /** this.carried */
-    let carried = this.carriedValue ? this.carriedValue : null;
-    let carriedObj = { carriedValue: carried };
 
     /**
      *  exit condition
-     *  call fnFinal/fnFinalCatch and exit the recursion.
+     *  call fn/fnCatch and exit the recursion.
      */
-    if (this.params.length === 0) {
-      try {
-        this.fnFinal.call(carriedObj);
-      } catch (err) {
-        if (typeof this.fnFinalCatch !== "function")
-          throw new Error(
-            `finalAry.fnFinalCatch has to be of a type function.`
-          );
-        this.fnFinalCatch.call({ err: err });
-      }
-      return;
-    }
-
-    if (functions.length === 0) {
-      param = params.pop();
-      functions = [...this.functionAry];
-    }
-
     const fns = functions.pop();
 
-    const pfn = Promise.resolve(fns.fn.call(carriedObj, param));
+    const pfn = Promise.resolve(fns.fn.call({ carriedValue: this.returns }, param));
     pfn
-      .then((res) => {
-        carried = res ? res : carried;
-        carriedObj = { carriedValue: carried };
-        this._recur.call(carriedObj, params, functions);
+      .then(res => {
+        if (res) this.returns = [...this.returns, res];
+
+        if (this.params.length === 0 && functions.length === 0) {
+          try {
+            const pfn = Promise.resolve(this.fnFinal.call({ carriedValue: this.returns }, param));
+            pfn.then(res => {
+              return;
+            });
+          } catch (err) {
+            if (typeof this.fnFinalCatch !== "function") throw new Error(`final.fnCatch has to be a type function.`);
+            this.fnFinalCatch.call({ err: err });
+          }
+          return;
+        }
+
+        if (functions.length === 0) {
+          param = this.params.pop();
+          functions = [...this.functionAry];
+        }
+        this._recur.call({ carriedValue: this.returns }, param, functions);
       })
-      .catch((err) => {
-        console.log(`failed to resolve fn`, err);
+      .catch(err => {
+        const pfnCatch = Promise.resolve(fns.fnCatch.call({ err: err }, param));
+        pfnCatch.then(res => {
+          return;
+        });
       });
   };
 
+  /**
+   * Retrieves the carried value from the series of function executions.
+   * @returns {*} The carried value from the series of function executions.
+   */
   getCarriedValue = () => {
     return this.carried;
   };
 
-  _isFnStatement = (fn) => {
+  /**
+   * Checks if the provided function is a function statement or an arrow function.
+   * @private
+   * @param {Function} fn - The function to be checked.
+   * @returns {boolean} `true` if the function is a statement, `false` if it's an arrow function.
+   */
+  _isFnStatement = fn => {
     const regex = new RegExp(/^\s*function\s/);
     if (regex.test(fn)) return true;
 
@@ -94,3 +110,5 @@ class FnChain {
     }
   };
 }
+
+module.exports = FnChain;
--- threadsOrder.js
+++ threadsOrder.js
@@ -1,126 +1,232 @@
 /**
- * Class representing a utility for running functions concurrently with a limited number of threads.
+ * Represents a chain of functions/promises executed sequentially with parameters provided.
  * @class
- *
- *
- *
+ * @classdesc The `FnChain` class iterates through parameters and executes a series of
+ * functions/promises with each parameter. The result of each execution is carried over
+ * to the next iteration. After all parameters are processed, it optionally executes a final
+ * function/promise provided in the `finalAry`.
  */
-class Threads {
+class ThreadsOrdered {
   /**
-   * Create a Threads instance.
-   * @constructor
-   * @param {number} threadNum - The maximum number of tasks running simultaneously.
-   * @param {Array} baseArgs - An array of base arguments that will be passed to each function.
-   * @param {Array} fnArray - An array of objects containing functions to be executed and their corresponding callbacks.
-   * @param {Function} [finalCb=() => {}] - The final callback function to be executed when all tasks are completed.
-   * @param {Array} [finalCbArgs=[]] - Arguments to be passed to the final callback function.
+   * Serialized promise container.  Hold promises in an array of object {id:integer, prms:[promises]}
    */
-  constructor(threadNum, baseArgs, functions) {
-    /** Max number of tasks running simultaneously */
-    this.threadNum = threadNum;
+  static prmsAry = [];
+  /**
+   * Creates an instance of the `FnChain` class.
+   * @constructor
+   * @param {Number} threads - Number of threads i.e. number of promises running concurrently.
+   * @param {Array} paramAry - An array of parameters to be used as arguments in function execution.
+   * @param {Array} functionAry - An array of functions/promises to be executed in sequence.
+   * @param {Object} [final] - An optional object containing finalization functions.
+   * @param {Function} [final.fn] - The final function to be executed after processing all parameters.
+   * @param {Function} [final.fnCatch] - The function to handle errors in the finalization process.
+   * @throws Will throw an error if `paramAry` or `functionAry` is empty.
+   */
+  constructor(threads, paramAry, functionAry, finalObj) {
+    if (paramAry.length === 0) throw new Error(`Parameter array is empty.`);
+    if (functionAry.length === 0) throw new Error(`Function array is empty.`);
 
-    /** Array container for the arguments to be used for the series of functions  */
-    this.baseArgs = baseArgs.reverse(); // reverse order for the purpose of using pop instead of shift
+    this.threads = threads;
 
-    /**
-     * An array of objects, each specifying a set of functions and callbacks.
-     * @type {Array<FunctionArrayObject>}
-     * Represents an array of objects, each specifying a set of functions and callbacks.
-     * @typedef {Object} FunctionArrayObject
-     * @property {Function} fn - The main function to be executed.
-     * @property {Function} [fnCatch] - The callback function to be executed after the main function.
-    *  @property {Function} [fnCb] - The callback function to be executed after the main function.
-     * @property {Function} [fnCbCatch] - The catch function to be executed when the main function fails.
-     * @property {Function} [fn] - The catch function to be executed when the callback function fails.
-     * @property {Function} [fnFinal] - The catch function to be executed when the callback function fails.
-     * @property {Function} [fnFinalCatch] - The catch function to be executed when the callback function fails.
+    /** Array of parameters to be passed to the functions as arguments.  Reversed for the purpose of using pop */
+    this.params = paramAry.reverse();
 
-    */
-    this.fnObj = fnObj;
+    /** Series of funtions/promises.  Functions/promises runs in the order from functionAry[0].  Reserved for the purpose of using pop */
+    this.functionAry = functionAry.reverse();
 
-    /** Tracks the number of remaining tasks. */
-    this.remainingTasks = this.baseArgs.length;
+    /** Holds returned/resolved values pased by functions/promises.  Functions/promises can refer to this value as this.carriedValue. */
+    this.returns = [];
 
-    /** define functions */
-    Object.keys(functions).forEach((key) => {
-      this[key] = key ? functions.key : () => {};
-    });
+    /** Track the next promise to be resolved. */
+    this.resolveId = 0;
 
-    /** Set of functions for the initial run */
+    /** id counter for serializing promises */
+    this.idCnt = 0;
 
-    /** Initialize thread.  Promises/functions up to threadNum runs concurrently */
-    for (let n = 0; n < this.threadNum; n++) {
-      /** Do nothing when threadNum is larger than the length of baseArg */
-      if (this.baseArgs.length === 0) {
-        return;
+    this.functionAry = functionAry;
+    if (finalObj) {
+      this.fnFinal = finalObj.fn ? finalObj.fn : function () {};
+      this.fnFinalCatch = finalObj.fnCatch ? finalObj.fnCatch : function () {};
+      if (typeof this.fnFinal !== "function" || typeof this.fnFinalCatch !== "function") {
+        throw new Error(`FnFinal and fnFinalCatch both need to be functions.${typeof this.fnFinal}, ${typeof this.fnFinalCatch}`);
       }
-      /** Start calling functions in sequence. */
-      this._runThread();
+
+      for (let i = 0; i < this.threads; i++) {
+        if (this.params.length === 0) {
+          return;
+        }
+        const param = this.params.pop();
+        this._prepFunctions(param);
+      }
+
+      this._resolver();
     }
   }
 
-  /**
-   * Run a task in a thread.
-   * @private
-   */
-  _runThread = () => {
-    if (this.baseArgs.length === 0) {
-      return;
-    }
-
-    const args = this.baseArgs.pop();
-    const fns = [...this.fnArray];
-
-    this._callFnInSequence(args, fns);
+  _prepFunctions = param => {
+    let resultObj = { id: this.idCnt, param: param, fn: [] };
+    this.functionAry.forEach(fns => {
+      let obj = {};
+      obj.prms = this._isPromise(fns.fn) ? fns.fn : this._promisify(fns.fn);
+      if (fns.fnCatch) obj.fnCatch = fns.fnCatch;
+      if (fns.cb) obj.cb = fns.cb;
+      if (fns.cbCatch) obj.cbCatch = fns.cbCatch;
+      resultObj.fn.push(obj);
+    });
+    ThreadsOrdered.prmsAry.push(resultObj);
+    this.idCnt++;
   };
 
   /**
-   * Call functions in sequence with the provided arguments.
+   * Internal recursive function for executing functions/promises in sequence with parameters.
    * @private
-   * @param {any} args - The arguments to be passed to the functions.
-   * @param {Array} fns - An array of objects containing functions and their corresponding callbacks.
-   * @returns {Promise} - A promise that resolves when all functions and callbacks are completed.
+   * @param {*} param - The current parameter to be used as an argument.
+   * @param {Array} functions - The functions/promises to be executed in a sequence.
    */
+  _resolver = () => {
+    /** this.carried */
 
-  _callFnInSequence = async (args, fns) => {
-    let skipCb = false;
-    let fnReturn;
-    const fnObj = fns.shift();
-    try {
-      const pfn = Promise.resolve(fnObj.fn(args));
-      fnReturn = await pfn;
-    } catch (err) {
-      console.error(`failed to resolve ${fnObj.fn}`);
-      skipCb = true;
-    }
+    /**
+     *  exit condition
+     *  call fn/fnCatch and exit the recursion.
+     */
+    let param = ThreadsOrdered.prmsAry[this.resolveId].param;
+    let fns = ThreadsOrdered.prmsAry[this.resolveId].fn.pop();
 
-    if (skipCb === false) {
-      try {
-        const pfncb = Promise.resolve(fnObj.fnCb(args, fnReturn));
-        await pfncb;
-      } catch (err) {
-        console.error(`failed to resolve ${fnObj.fnCb}`);
-      }
-    }
-    if (fns.length === 0) {
-      this.remainingTasks--;
+    fns.prms
+      .call({ carriedValue: this.returns }, param)
+      .then(fnReturn => {
+        if (fns.cb) {
+          if (fnReturn) this.returns = [...this.returns, fnReturn];
 
-      if (this.remainingTasks === 0) {
-        this.finalCb();
-        return false;
-      }
+          const pcb = this._isPromise(fns.cb) ? fns.cb : this._promisify(fns.cb);
+          pcb
+            .call({ carriedValue: this.returns }, param)
+            .then(cbReturn => this._thenTask(cbReturn))
+            .catch(err => {
+              if (fns.cbCatch) {
+                this._catchTask(err, fns.cbCatch, param);
+              } else {
+                return;
+              }
+            });
+        } else {
+          this._thenTask(fnReturn);
+        }
+      })
+      .catch(err => {
+        if (fns.fnCatch) {
+          this._catchTask(err, fns.fnCatch, param);
+        } else {
+          return;
+        }
+      });
+  };
 
-      if (this.baseArgs.length === 0) {
-        return false;
-      }
+  _thenTask = returned => {
+    if (returned) this.returns = [...this.returns, returned];
+    //this.resolveId++;
+    this._next();
+  };
 
-      args = this.baseArgs.shift();
-      fns = [...this.fnArray];
-      this._callFnInSequence(args, fns);
+  _catchTask = (err, fn, param) => {
+    this.returns = [...this.returns, err];
+    // const pCatch = Promise.resolve(fn.call({ err: err }, param));
+    // pCatch.then(returned => this._thenTask(returned));
+    const pfn = this._isPromise(fn) ? fn : this._promisify(fn);
+    pfn
+      .call({ err: err }, param)
+      .then(res => this._thenTask(res))
+      .catch(err => this._catchTask(err));
+  };
+
+  _next = () => {
+    const isMoreTask = ThreadsOrdered.prmsAry.length - 1 > this.resolveId;
+    const isMoreFunctions = ThreadsOrdered.prmsAry[this.resolveId].fn.length > 0;
+    const isMoreParams = this.params.length > 0;
+    if (!isMoreTask && !isMoreFunctions && !isMoreParams) {
+      this._final();
+    } else if (isMoreTask && !isMoreFunctions && isMoreParams) {
+      this.resolveId++;
+      const p = this.params.pop();
+      this._prepFunctions(p);
+      this._resolver();
+    } else if (isMoreTask && !isMoreFunctions) {
+      this.resolveId++;
+      this._resolver();
     } else {
-      this._callFnInSequence(args, fns);
+      this._resolver();
     }
+  };
+
+  _final = () => {
+    // console.log(this._isPromise(this.fnFinal), this._isPromise(this.fnFinalCatch));
+    try {
+      const pfinal = this._isPromise(this.fnFinal) ? this.fnFinal : this._promisify(this.fnFinal);
+      pfinal.call({ carriedValue: this.returns }).then(res => {
+        return;
+      });
+    } catch (err) {
+      const pfinalCatch = this._isPromise(this.fnFinalCatch) ? this.fnFinalCatch : this._promisify(this.fnFinalCatch);
+      pfinalCatch.then(res => {
+        return;
+      });
+    }
+  };
+
+  /**
+   * Retrieves the carried value from the series of function executions.
+   * @returns {*} The carried value from the series of function executions.
+   */
+  getCarriedValue = () => {
+    return this.carried;
+  };
+
+  /**
+   * Checks if the provided function is a function statement or an arrow function.
+   * @private
+   * @param {Function} fn - The function to be checked.
+   * @returns {boolean} `true` if the function is a statement, `false` if it's an arrow function.
+   */
+  // _isFnStatement = fn => {
+  //   const regex = new RegExp(/^\s*function\s/);
+  //   if (regex.test(fn)) return true;
+
+  //   try {
+  //     Function("return " + fn);
+  //     return false;
+  //   } catch (error) {
+  //     return true;
+  //   }
+  // };
+
+  _isPromise = fn => {
+    const regex = new RegExp(/new Promise\(/);
+    if (typeof fn === "function") {
+      // Check if the result is a promise
+      if (regex.test(fn)) {
+        return true;
+      } else {
+        return false;
+      }
+    } else if (fn && typeof fn.then === "function") {
+      return true;
+    } else {
+      throw new Error(`${fn} has to be either a function or a promise.`);
+    }
+  };
+
+  _promisify = fn => {
+    return function () {
+      return new Promise((resolve, reject) => {
+        try {
+          const result = fn.apply(this, arguments);
+          resolve(result);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    };
   };
 }
-
-module.exports = Threads;
+module.exports = ThreadsOrdered;
